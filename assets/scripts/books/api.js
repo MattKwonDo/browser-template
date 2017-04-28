2a5eab07fd802dc514e441498968420c1bee6049
MattKwonDo <mgduvall@gmail.com>
2017-04-28 16:10:00 -0400

    add placeholders for create, update, read to api

--- assets/scripts/books/api.js
+++ assets/scripts/books/api.js
@@ -4,6 +4,7 @@ const app = require('../app.js')
 const store = require('../store')
 // const gameStore = require('../gameStore')
 
+// read
 const getBooks = function () {
   return $.ajax({
     url: app.host + '/new_items', // "http://book-json.herokuapp.com/books"
@@ -14,6 +15,59 @@ const getBooks = function () {
   })
 }
 
+// create
+// need to update this to do an ajax method to delete the thing from the database
+const createItems = function () {
+  return $.ajax({
+    url: app.host + '/new_items', // "http://book-json.herokuapp.com/books"
+    method: 'POST',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: {
+      'new_item': {
+        'name': 'ajax_glove'
+      }
+    }
+  })
+}
+
+// update
+// need to update this to do an ajax method to delete the thing from the database
+// need id included
+const updateItems = function () {
+// need ID here
+  return $.ajax({
+    url: app.host + '/new_items/' + ID, // "http://book-json.herokuapp.com/books"
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: {
+      'new_item': {
+        'name': 'ajax_glove'
+      }
+    }
+  })
+}
+
+// delete
+// need to update this to do an ajax method to delete the thing from the database
+// need id included
+const deleteItems = function () {
+// need ID here
+  return $.ajax({
+    url: app.host + '/new_items', // "http://book-json.herokuapp.com/books"
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
 module.exports = {
-  getBooks
+  getBooks,
+  createItems,
+  updateItems,
+  deleteItems
 }
