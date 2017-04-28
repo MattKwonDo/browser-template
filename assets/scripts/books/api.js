f8ed3466da186a070b4c5c4bffe1ae6ba3995341
MattKwonDo <mgduvall@gmail.com>
2017-04-28 11:49:15 -0400

    add token to getBooks to get items from server

--- assets/scripts/books/api.js
+++ assets/scripts/books/api.js
@@ -1,11 +1,16 @@
 'use strict'
 
 const app = require('../app.js')
+const store = require('../store')
+// const gameStore = require('../gameStore')
 
 let getBooks = function () {
   return $.ajax({
     url: app.host + '/new_items', // "http://book-json.herokuapp.com/books"
-    method: 'GET'
+    method: 'GET',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
   })
 }
 
