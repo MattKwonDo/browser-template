4cf9d9cad877bfaa0d42d52de3947316618acf40
MattKwonDo <mgduvall@gmail.com>
2017-05-01 18:47:37 -0400

    update config routes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict'
 
 module.exports = {
-  host: 'https://ski-list.herokuapp.com/'
+  host: 'https://ski-list.herokuapp.com'
   // host: 'https://wdi-my-library.herokuapp.com',
 }
--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -4,7 +4,7 @@ const config = {
   apiOrigins: {
     development: 'http://localhost:4741',
     // change to this when move to deploy
-    production: 'https://ski-list.herokuapp.com/'
+    production: 'https://ski-list.herokuapp.com'
     // production: 'http://localhost:4741'
 
     // https://ga-wdi-boston.herokuapp.com'
