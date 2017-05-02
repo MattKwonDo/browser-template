f1878dc414c18f161beae9b1e2c0acc646b28907
MattKwonDo <mgduvall@gmail.com>
2017-05-02 11:08:06 -0400

    update config for deploy

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -4,10 +4,12 @@ const config = {
   apiOrigins: {
     // development: 'http://localhost:4741',
 // change this before deploy
-    // production: 'https://ski-list.herokuapp.com'
-    production: 'http://localhost:4741'
+    production: 'https://ski-list.herokuapp.com'
+    // production: 'http://localhost:4741'
   },
-  host: 'http://localhost:4741'
+// change this before deploy
+  host: 'https://ski-list.herokuapp.com'
+  // host: 'http://localhost:4741'
 }
 
 module.exports = config
